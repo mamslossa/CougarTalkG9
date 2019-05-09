e4ee4cc342cbd952bf799e345120bf52ab84e68d
Florian Vincent <florian.vincent@epitech.eu>
2019-05-09 12:26:58 -0700

    Desactivate SPring boot database management

--- src/main/java/com/plkpiotr/forum/ForumApplication.java
+++ src/main/java/com/plkpiotr/forum/ForumApplication.java
@@ -13,16 +13,20 @@ import com.plkpiotr.forum.repositories.TopicRepository;
 import com.plkpiotr.forum.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.List;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class ForumApplication {
 
     @Bean
